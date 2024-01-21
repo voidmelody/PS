7882520ef69571d20bff21bda868ce4e5a69ff83
voidmelody <sss9073@naver.com>
2024-01-21 20:01:41 +0900

    Feat: bfs (1987)

--- baekjoon/1987.py
+++ baekjoon/1987.py
@@ -1,30 +1,23 @@
-from collections import deque
+R, C = map(int, input().split())
+graph = []
+for _ in range(R):
+    graph.append(list(input()))
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
 def bfs():
-    # q 등 필요데이터 생성
-    q = deque()
-    # v = [[[] for _ in range(C)] for _ in range(R)] # 리스트는 O(N)
-    v = [[set() for _ in range(C)] for _ in range(R)] # set는 O(1)
+    s = set()
+    s.add((0, 0, graph[0][0]))
     ans = 1
-
-    # q에 초기데이터(들) 삽입
-    q.append((0,0,arr[0][0]))
-    v[0][0].add(arr[0][0])
-
-    while q:
-        ci,cj,cv = q.popleft()
-        ans = max(ans, len(cv))
-        # 4방향, 범위내, 중복값이 아닌경우, 중복시퀀스 아닌경우
-        for di,dj in ((-1,0),(1,0),(0,-1),(0,1)):
-            ni,nj = ci+di, cj+dj
-            if 0<=ni<R and 0<=nj<C and arr[ni][nj] not in cv:
-                if cv+arr[ni][nj] not in v[ni][nj]:
-                    q.append((ni,nj,cv+arr[ni][nj]))
-                    v[ni][nj].add((cv+arr[ni][nj]))
+    while s:
+        x, y, c = s.pop()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < R and 0 <= ny < C:
+                if graph[nx][ny] not in c:
+                    s.add((nx, ny, c + graph[nx][ny]))
+                    ans = max(ans, len(c) + 1)
     return ans
 
-
-R, C = map(int, input().split())
-arr = list(input() for _ in range(R))
-
-ans = bfs()
-print(ans)
+print(bfs())
